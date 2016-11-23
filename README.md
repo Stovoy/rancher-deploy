6bc5018aab139fd7913d002825af58723e4f33b2
Steve Mostovoy <stevemostovoysm@gmail.com>
2016-11-22 19:49:32 -0800

    Make it work.

--- README.md
+++ README.md
@@ -1,32 +1,58 @@
 [![License](http://img.shields.io/:license-mit-blue.svg)](http://doge.mit-license.org)
-## rancher-deploy.sh
+# rancher-deploy.sh
 
 Does a Rancher upgrade.
 
 You will need to create an API access token and secret for Rancher.
 
-The rancher URL needs to be your full url including project id. You will get the url on your api key page. Make sure you are using the environment you want to use. Do not include http/s in the url. There is an option for https.
+If you wanted to deploy swaggy/awesome:3.0, you'd set `DOCKER_IMAGE=swaggy/awesome`, and `TAG=3.0`.
 
-If I wanted to deploy swaggy/awesome:3.0 I would use `docker_org: swaggy`, `docker_image: awesome`, and `tag: 3.0`.
+If your tag is unique each deploy, you should set `USE_TAG` to true. But if you always deploy the same tag (i.e. latest), you should set `use_tag` to false. `use_tag` just sets the rancher service name to include the tag, for example awesome-3.0 -> awesome-3.1. But if your tag is not unique, then a random number will be generated for you. Rancher enforces that the service has to have a new unique name on every deploy.
 
-If your tag is unique each deploy, you should set `use_tag` to true. But if you always deploy the same tag (i.e. latest), you should set `use_tag` to false. `use_tag` just sets the rancher service name to include the tag, for example awesome-3.0 -> awesome-3.1. But if your tag is not unique, then a random number will be generated for you. Rancher enforces that the service has to have a new unique name on every deploy.
+If you use in-place mode, then you don't need new unique names. Just set `USE_TAG` to false and `IN_PLACE` to true.
 
-If you use inplace mode, then you don't need new unique names. Just set `use_tag` to false and `inplace` to true.
+The rancher URL needs to be your full url including the project id. You can get the url on your api key page - it's the highlighted text next to "Endpoint". Make sure you create the api key in the environment you want to use. 
+Note: Don't include http/s:// in the url - it's added through a flag (HTTPS).
 
-Set up your environmental variables for your use case:
+## Necessary packages
+* jq
+* unzip
+
+## Necessary variables
+* ACCESS_KEY
+* SECRET_KEY
+* RANCHER_URL
+* STACK_NAME
+* SERVICE_NAME
+* DOCKER_ORG
+* DOCKER_IMAGE
+
+## Optional variables
+* HTTPS (default: true)
+* TAG (default: latest)
+* USE_TAG (default: true)
+* IN_PLACE (default: true)
+* START_FIRST (default: true)
+* RANCHER_COMPOSE_TAR (default: https://releases.rancher.com/compose/v0.8.6/rancher-compose-linux-amd64-v0.8.6.tar.gz)
+* CLEANUP (default: true)
+
+## Example environment
 ```
-access_key: $RANCHER_ACCESS_KEY
-secret_key: $RANCHER_SECRET_KEY
-rancher_url: $RANCHER_URL
-https: false
-tag: latest
-stack_name: my-stack
-service_name: awesome
-docker_org: swaggy
-docker_image: awesome
-use_tag: false
-inplace: true
-start_first: true
+ACCESS_KEY=$RANCHER_ACCESS_KEY
+SECRET_KEY=$RANCHER_SECRET_KEY
+RANCHER_URL=$RANCHER_URL
+HTTPS=false
+TAG=latest
+STACK_NAME=my-stack
+SERVICE_NAME=awesome
+DOCKER_ORG=swaggy
+DOCKER_IMAGE=awesome
+USE_TAG=false
+IN_PLACE=true
+START_FIRST=true
 ```
-Running it is simple.
-`curl https://github.com/swaggy/rancher-deploy/blob/master/run.sh | sh`
+
+## Running it
+After your environmental variables are set, you can run it like this:
+
+`curl https://github.com/swaggy/rancher-deploy/blob/master/rancher-deploy.sh | sh`
---
+++ rancher-deploy.sh
@@ -0,0 +1,142 @@
+#!/bin/bash -ex
+
+# Needs `unzip`, `jq` packages.
+if ! which unzip > /dev/null; then
+    echo "rancher-deploy.sh needs unzip." 2>&1
+    exit 1
+fi
+
+if ! which jq > /dev/null; then
+    echo "rancher-deploy.sh needs jq." 2>&1
+    exit 1
+fi
+
+# Check necessary variables.
+declare -a WANTED_VARS=(
+    "ACCESS_KEY"
+    "SECRET_KEY"
+    "RANCHER_URL"
+    "STACK_NAME"
+    "SERVICE_NAME"
+    "DOCKER_IMAGE")
+
+missing=false
+for VAR in "${WANTED_VARS[@]}"; do
+    if [ -z "${!VAR}" ]; then
+        echo "Missing $VAR."
+        missing=true
+    fi
+done
+if [[ $missing == true ]]; then
+    exit 1
+fi
+
+# Parse optional variables:
+# HTTPS (default: true)
+# TAG (default: latest)
+# USE_TAG (default: true)
+# IN_PLACE (default: true)
+# START_FIRST (default: true)
+# RANCHER_COMPOSE_TAR (default: https://releases.rancher.com/compose/v0.8.6/rancher-compose-linux-amd64-v0.8.6.tar.gz)
+# CLEANUP (default: true)
+
+if [[ -z "$HTTPS" || "$HTTPS" == true ]]; then
+    PROTOCOL=https
+elif [[ "$HTTPS" == false ]]; then
+    PROTOCOL=http
+else
+    echo "Invalid value for HTTPS: $HTTPS" 2>&1
+    exit 1
+fi
+
+if [[ -z "$TAG" ]]; then
+    TAG=latest
+else
+    TAG="$TAG"
+fi
+
+if [[ -z "$USE_TAG" || "$USE_TAG" == true ]]; then
+    SUFFIX="$TAG"
+elif [[ "$USE_TAG" == false ]]; then
+    SUFFIX=$RANDOM
+else
+    echo "Invalid value for USE_TAG: $USE_TAG" 2>&1
+    exit 1
+fi
+
+if [[ -z "$IN_PLACE" || "$IN_PLACE" == true ]]; then
+    IN_PLACE=true
+elif [[ "$IN_PLACE" != false ]]; then
+    echo "Invalid value for IN_PLACE: $IN_PLACE" 2>&1
+    exit 1
+fi
+
+if [[ -z "$START_FIRST" || "$START_FIRST" == true ]]; then
+    START_FIRST=true
+elif [[ "$START_FIRST" != false ]]; then
+    echo "Invalid value for START_FIRST: $START_FIRST" 2>&1
+    exit 1
+fi
+
+RANCHER_COMPOSE_TAR=${RANCHER_COMPOSE_TAR:-'https://releases.rancher.com/compose/v0.8.6/rancher-compose-linux-amd64-v0.8.6.tar.gz'}
+
+if [[ -z "$CLEANUP" || "$CLEANUP" == true ]]; then
+    CLEANUP=true
+elif [[ "$CLEANUP" != false ]]; then
+    echo "Invalid value for CLEANUP: $CLEANUP" 2>&1
+    exit 1
+fi
+
+echo "Getting rancher compose executable."
+
+# Strip directory to put rancher-compose directly in current directory.
+curl "$RANCHER_COMPOSE_TAR" | tar xvz --strip-components=2
+if [[ "$CLEANUP" == true ]]; then
+    trap 'rm -f docker-compose' ERR EXIT
+fi
+
+ENV_ID=$(curl -s "$PROTOCOL://$ACCESS_KEY:$SECRET_KEY@$RANCHER_URL/environments?name=$STACK_NAME" | jq '.data[0].id' | sed s/\"//g)
+
+wget -qO file.zip "$PROTOCOL://$ACCESS_KEY:$SECRET_KEY@$RANCHER_URL/environments/$ENV_ID/composeconfig"
+if [[ "$CLEANUP" == true ]]; then
+    trap 'rm -f file.zip' ERR EXIT
+fi
+unzip -o file.zip
+if [[ "$CLEANUP" == true ]]; then
+    trap 'rm -f docker-compose.yml rancher-compose.yml' ERR EXIT
+fi
+
+if [[ "$IN_PLACE" != true ]]; then
+    OLD_SERVICE_NAME=$(sed -n "s/^\($SERVICE_NAME[^:]*\):[\r\n]$/\1/p" docker-compose.yml)
+fi
+
+
+if [[ "$IN_PLACE" != true ]]; then
+    # Update docker-compose.yml to include new service name
+    sed -i "s/^$OLD_SERVICE_NAME:/$OLD_SERVICE_NAME:\r\n$SERVICE_NAME-$SUFFIX:/g" docker-compose.yml
+    sed -i "s/^$OLD_SERVICE_NAME:/$OLD_SERVICE_NAME:\r\n$SERVICE_NAME-$SUFFIX:/g" rancher-compose.yml
+fi
+
+# Update image in docker-compose.yml.
+ESCAPED_DOCKER_IMAGE=$(echo "$DOCKER_IMAGE" | sed 's/\//\\\//g')  # Escape backslashes.
+sed -i "s/^\(\s *image: $ESCAPED_DOCKER_IMAGE\).*$/\1:$TAG/g" docker-compose.yml
+
+if [[ "$START_FIRST" == true ]]; then
+    # Add start first directive to rancher-compose.yml.
+    sed -i "s/\(^[a-zA-Z].*:\)/\1\r\n  upgrade_strategy:\r\n    start_first: true/g" rancher-compose.yml
+fi
+if [[ "$IN_PLACE" == true ]]; then
+    echo "Starting upgrade..."
+    ./rancher-compose --url "$PROTOCOL://$RANCHER_URL" --access-key "$ACCESS_KEY" --secret-key "$SECRET_KEY" --project-name "$STACK_NAME" up -d --upgrade --pull --interval 30000 --batch-size 1 "$SERVICE_NAME"
+    echo "Done."
+    if [[ "$START_FIRST" == false ]]; then
+        # Have to wait before confirming because the pull and launch will happen asyncronously.
+        echo "Waiting 60 seconds to confirm upgrade..."
+        sleep 60
+    fi
+    echo "Confirming upgrade..."
+    ./rancher-compose --url "$PROTOCOL://$RANCHER_URL" --access-key "$ACCESS_KEY" --secret-key "$SECRET_KEY" --project-name "$STACK_NAME" up -d --upgrade --confirm-upgrade "$SERVICE_NAME"
+    echo "Done."
+else
+    ./rancher-compose --url "$PROTOCOL://$RANCHER_URL" --access-key "$ACCESS_KEY" --secret-key "$SECRET_KEY" --project-name "$STACK_NAME" upgrade "$OLD_SERVICE_NAME" "$SERVICE_NAME-$SUFFIX" --pull --update-links -c --interval 30000 --batch-size 1
+fi
